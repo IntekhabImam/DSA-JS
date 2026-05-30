cbbe4e32e5567a7cf70ed62d2a45d2a659e1019e
Intekhab Imam <111379442+IntekhabImam@users.noreply.github.com>
2026-05-31 00:23:13 +0530

    infinity change

--- largest_elem.js
+++ largest_elem.js
@@ -1,5 +1,7 @@
 function largestElem(arr){
-    let maxElem = -1;
+
+    let maxElem = -Infinity; //this is the least min number
+
     for(let i = 0; i<arr.length; i++){
         if(arr[i] > maxElem){
             maxElem = arr[i];
@@ -8,7 +10,7 @@ function largestElem(arr){
     return maxElem;
 }
 
-let arr = [5,4,7,30,6];
+let arr = [5,-1,7,9,2];
 let res = largestElem(arr);
 
 console.log(res);
